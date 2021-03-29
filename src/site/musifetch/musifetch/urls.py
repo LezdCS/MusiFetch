9eede3b93a2b3a691b8254ef7cf18326c4a2d8ed
GroGGy67 <arnaudschmeltz@outlook.fr>
2021-03-29 20:50:23 +0200

    register + login + logout + navs

--- src/site/musifetch/musifetch/urls.py
+++ src/site/musifetch/musifetch/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('find/', views.find, name='find'),
     path('create/', views.create, name='create'),
     path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 
     path('api/', include('api_musifetch.urls')),
 ]
--- src/site/musifetch/musifetch/views.py
+++ src/site/musifetch/musifetch/views.py
@@ -1,13 +1,56 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.core.validators import *
 import sys
+from fingerprints import fingerprints_generator
 
 sys.path.append("..")  # Adds higher directory to python modules path.
 
-from fingerprints import fingerprints_generator
+
+# routes
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['login']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/home')
+        else:
+            return render(request, 'home/login.html', {'error': 'Invalid username or password, please try again'})
+    return render(request, 'home/login.html')
 
 
 def register(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        donnees = [email, username, password, confirm_password]
+        errors = validatorRegister(donnees)
+        if errors == {}:
+            try:
+                User.objects.create_user(username, email, password)
+                messages.success(request, 'Account was successfully created. Welcome, ' + username + '!')
+                return redirect('/home')
+            except:
+                html = "<html><body>Erreur inconnue</body></html>"
+                return HttpResponse(html)
+        else:
+            errors['keep_email'] = email
+            errors['keep_username'] = username
+            return render(request, 'home/register.html', errors)
     return render(request, 'home/register.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/home')
 
 
 def find(request):
@@ -49,3 +92,31 @@ def create(request):
         #     algo = fingerprints_generator.Algo()
         #     algo.choice("createPlaylist",playlist_link)
     return redirect('/home')
+
+
+# external functions
+
+def validatorRegister(donnees):
+    errors = {}
+
+    # email
+    try:
+        validate_email(donnees[0])
+    except ValidationError:
+        errors['email_error'] = 'Please enter a valid email address'
+
+    # username
+    if len(donnees[1]) < 4:
+        errors['username_error'] = 'Username must contain at least 4 character'
+    elif len(donnees[1]) > 16:
+        errors['username_error'] = 'Username cannot contain more than 16 character'
+
+    # password
+    if len(donnees[2]) < 6:
+        errors['password_error'] = 'Password must contain at least 6 character'
+
+    # confirm password
+    if donnees[2] != donnees[3]:
+        errors['confirm_password_error'] = 'Passwords do not match'
+
+    return errors
