5c472caa589c305f3ec7bc63f94444bee85749ea
LezdCS <63884731+LezdCS@users.noreply.github.com>
2021-03-29 16:43:39 +0200

    Improving create view

--- src/site/musifetch/musifetch/views.py
+++ src/site/musifetch/musifetch/views.py
@@ -27,16 +27,25 @@ def find(request):
 
 def create(request):
     if request.method == 'GET':
-        return render(request, 'home/create.html', {})
+        return render(request, 'home/create.twig', {})
 
     if request.method == 'POST':
 
-        ytb_link = request.POST['video_link']
-        playlist_link = request.POST['playlist_link']
-        if (ytb_link != ""):
+        type_link = request.POST['typeLink']
+
+        if type_link == "videoLink":
             algo = fingerprints_generator.Algo()
-            algo.choice("create", ytb_link)
-        elif (playlist_link != ""):
+            algo.choice("create", request.POST['link'])
+        elif type_link == "playlistLink":
             algo = fingerprints_generator.Algo()
-            algo.choice("createPlaylist",playlist_link)
+            algo.choice("createPlaylist", request.POST['link'])
+
+        # ytb_link = request.POST['video_link']
+        # playlist_link = request.POST['playlist_link']
+        # if (ytb_link != ""):
+        #     algo = fingerprints_generator.Algo()
+        #     algo.choice("create", ytb_link)
+        # elif (playlist_link != ""):
+        #     algo = fingerprints_generator.Algo()
+        #     algo.choice("createPlaylist",playlist_link)
     return redirect('/home')
